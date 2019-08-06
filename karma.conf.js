63ce120be290cb809f2ffb9e720e0ac7bc83f434
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-06 16:53:56 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -14,9 +14,7 @@ module.exports = function(config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-phantomjs-launcher'),
-      require('@angular/cli/plugins/karma')
+      require('karma-jasmine-html-reporter')
     ],
 
     client: {
