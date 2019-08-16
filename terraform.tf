f8641f88c9acda041c529989b19a3d1107893623
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-16 15:24:22 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,6 +7,10 @@ resource "aws_instance" "AngulatTestDeploy" {
   instance_type = "t2.micro"
   key_name 	= "sanyam-terraform"
   
+  provisioner "file" {
+  source      = "/var/jenkins_home/workspace/Jenkins_Angular/dockerexec.sh"
+  destination = "/dockerexec.sh"
+}
 
 } 
 
