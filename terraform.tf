3c4b9bf4b066a870b4135ff4ddb89d09b112188e
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-16 15:05:03 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,15 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "AngulatTestDeploy" {
   ami           = "ami-0a313d6098716f372"
   instance_type = "t2.micro"
-  key_name 	= "sanyamkey"
+  key_name 	= "sanyam-terraform"
+  
+  provisioner "remote-exec" {
+    inline = [
+      
+    ]
+  }
 } 
+
