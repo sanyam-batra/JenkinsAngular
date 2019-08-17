ae8055eb158a9a905408f30b776432acba4e588b
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-17 15:20:37 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "AngulatTestDeploy" {
   ami           = "ami-004852354728c0e51"
   instance_type = "t2.micro"
   key_name 	= "sanyam-terraform"
+  
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    password = "ubuntu"
+  }
 
 } 
 
