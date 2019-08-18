8973912e8b9d9b8a345725d4c3893651040cee72
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-18 12:26:08 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,18 +1,12 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAI7VFL6GH3627IK7A"
-  secret_key = "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"
+  access_key = "AKIAJMKVMACSRQYAWGPA"
+  secret_key = "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"
 }
 
 resource "aws_instance" "AngulatTestDeploy" {
   ami           = "ami-035b3c7efe6d061d5"
   instance_type = "t2.micro"
   key_name 	= "sanyam-terraform"
-  
-  connection {
-    user = "ec2-user"
-    private_key = "${file("/private_key")}"
-    }
-
 } 
 
