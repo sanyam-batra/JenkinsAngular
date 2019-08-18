cf85791285569366c35aa9d92a8db8e8859612de
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-18 12:26:33 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,6 +7,6 @@ provider "aws" {
 resource "aws_instance" "AngulatTestDeploy" {
   ami           = "ami-035b3c7efe6d061d5"
   instance_type = "t2.micro"
-  key_name 	= "sanyam-terraform"
+  key_name 	= "terraform-jenkins"
 } 
 
