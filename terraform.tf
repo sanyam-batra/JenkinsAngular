a9093f59b69e3bf702215641ea172de3f34a53ff
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-18 14:11:28 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAJQ3SCF7WWXHYHZMQ"
-  secret_key = "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"
 }
 
 resource "aws_instance" "AngulatTestDeploy" {
