44dd17536617c9d1f81d713adb093878504af654
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-18 14:38:37 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,9 +2,75 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "AngulatTestDeploy" {
-  ami           = "ami-035b3c7efe6d061d5"
-  instance_type = "t2.micro"
-  key_name 	= "terraform-jenkins"
-} 
+resource "aws_vpc" "test-env" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "test-env"
+  }
+}
 
+
+resource "aws_subnet" "subnet-uno" {
+  cidr_block = "${cidrsubnet(aws_vpc.test-env.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.test-env.id}"
+  availability_zone = "us-east-1a"
+}
+
+
+resource "aws_security_group" "ingress-all-test" {
+name = "allow-all-sg"
+vpc_id = "${aws_vpc.test-env.id}"
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+resource "aws_instance" "test-ec2-instance" {
+  ami = "ami-035b3c7efe6d061d5"
+  instance_type = "t2.micro"
+  key_name = "terraform-jenkins(1)"
+  security_groups = ["${aws_security_group.ingress-all-test.id}"]
+  subnet_id = "${aws_subnet.subnet-uno.id}"
+}
+
+
+resource "aws_eip" "ip-test-env" {
+  instance = "${aws_instance.test-ec2-instance.id}"
+  vpc      = true
+}
+
+resource "aws_internet_gateway" "test-env-gw" {
+  vpc_id = "${aws_vpc.test-env.id}"
+tags {
+    Name = "test-env-gw"
+  }
+}
+
+resource "aws_route_table" "route-table-test-env" {
+  vpc_id = "${aws_vpc.test-env.id}"
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.test-env-gw.id}"
+  }
+tags {
+    Name = "test-env-route-table"
+  }
+}
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id      = "${aws_subnet.subnet-uno.id}"
+  route_table_id = "${aws_route_table.route-table-test-env.id}"
+}
