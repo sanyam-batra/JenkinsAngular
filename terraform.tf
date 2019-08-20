572b6661deaf792c6cd7cc1ae6fd39b46c395e9c
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-20 10:03:32 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -30,6 +30,18 @@ from_port = 22
     to_port = 22
     protocol = "tcp"
   }
+  
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+  }
+  
+  
+  
   egress {
    from_port = 0
    to_port = 0
