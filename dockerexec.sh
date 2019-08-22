dbbf54cdfdbc648d0679b41307890dff925dc3c0
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-22 15:26:51 +0530

    updating

--- dockerexec.sh
+++ dockerexec.sh
@@ -1,2 +1,2 @@
 docker pull sanyambatra13/jenkins-angularapp:ver6
-docker run -p 3000:3000 --rm sanyambatra13/jenkins-angularapp:ver6
+nohup docker run -p 3000:3000 --rm sanyambatra13/jenkins-angularapp:ver6 &
