aa13cedae68d7a9821eba9236a18d65ed06d5341
Sanyam Batra <sanyambatrs13@gmail.com>
2019-08-26 10:07:22 +0530

    Update dockerexec.sh

--- dockerexec.sh
+++ dockerexec.sh
@@ -1,2 +1,7 @@
-docker pull sanyambatra13/jenkins-angularapp:ver6
-nohup docker run -p 3000:3000 --rm sanyambatra13/jenkins-angularapp:ver6 &
+CONTAINER_NAME="angular_container"
+OLD="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME")"
+if [ -n "$OLD" ]; then
+  docker stop $OLD && docker rm $OLD
+fi
+docker pull sanyambatra13/jenkins-angularapp:latest
+nohup docker run --name angular_container -p 3000:3000 --rm sanyambatra13/jenkins-angularapp:latest &
